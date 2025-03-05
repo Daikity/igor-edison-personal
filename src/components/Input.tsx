2dbf09c1afec8516789c45cfb574477c760f9bc0
Igor Edison <roodyept@gmail.com>
2025-03-05 16:01:47 +0300

    send message form (#11)
    Co-authored-by: Igor Edison <igor.edison@kiwitaxi.com>

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,25 +1,27 @@
 'use client'
 
 import './scss/input.scss';
-import { texts } from '@/app/i18n';
-import Text from '@/components/Text'
+import Text from '@/components/Text';
 
-export default function Main({
-  label, name, type, placeholder}:
-  {label?: string, name: string, type: 'text' | 'textarea' | 'email' | 'password' | 'number',
-    placeholder?: string}
-) {
-  const { } = texts.en;
+interface InputProps {
+  label?: string;
+  name: string;
+  type: 'text' | 'textarea' | 'email' | 'password' | 'number';
+  placeholder?: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+}
 
+export default function Input({ label, name, type, placeholder, value, onChange }: InputProps) {
   return (
     <div className="input-container">
       <label>
-        {label ? <Text type='span' text={label} /> : null}
-        {
-          type != 'textarea' ?
-          <input type={type} name={name} placeholder={placeholder} /> :
-          <textarea name={name} placeholder={placeholder} />
-        }
+        {label && <Text type="span" text={label} />}
+        {type !== 'textarea' ? (
+          <input type={type} name={name} placeholder={placeholder} value={value} onChange={onChange} />
+        ) : (
+          <textarea name={name} placeholder={placeholder} value={value} onChange={onChange} />
+        )}
       </label>
     </div>
   );
--- src/sections/contacts.tsx
+++ src/sections/contacts.tsx
@@ -1,14 +1,34 @@
 'use client'
 
 import './scss/contacts.scss'
-import { RefObject } from 'react'
+import { RefObject, useState } from 'react'
 import { texts } from '@/app/i18n'
 import Text from '@/components/Text'
 import Input from '@/components/Input'
 import Button from '@/components/Button'
+import { FormData } from '@/app/types'
 
 export default function Contacts({ref}: {ref: RefObject<null>}) {
   const { contacts } = texts.en;
+
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const sendMessage = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("Отправленные данные:", formData);
+
+    fetch('/send-message', { method: 'POST', body: JSON.stringify(formData) });
+
+    setFormData({ name: "", email: "", message: "" }); // Очистка формы
+  };
 
   return (
     <div className="contacts" ref={ref}>
@@ -17,11 +37,11 @@ export default function Contacts({ref}: {ref: RefObject<null>}) {
           <Text type='h3' text={contacts.title} />
           <Text type='p' text={contacts.description} />
         </div>
-        <form>
-          <Input name='name' label={contacts.form.name} type='text' />
-          <Input name='email' label={contacts.form.email} type='email' />
-          <Input name='message' label={contacts.form.message} type='textarea' />
-          <Button text={contacts.form.button} />
+        <form onSubmit={sendMessage}>
+          <Input name="name" label={contacts.form.name} type="text" value={formData.name} onChange={handleChange} />
+          <Input name="email" label={contacts.form.email} type="email" value={formData.email} onChange={handleChange} />
+          <Input name="message" label={contacts.form.message} type="textarea" value={formData.message} onChange={handleChange} />
+          <Button text={contacts.form.button} type="submit" />
         </form>
       </div>
     </div>
